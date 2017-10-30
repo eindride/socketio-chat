ac0728e78e94cf838c80fdc6c2aa86d60a0065c3
Claudiu Neculau <claudiu.ncl97@gmail.com>
2017-10-30 11:01:07 +0200

    Default template for a react-app

---
+++ src/routes/profile/index.js
@@ -0,0 +1,10 @@
+import { h, Component } from 'preact';
+import style from './style';
+
+export default class Profile extends Component {
+	render({ user }, { time, count }) {
+		return (
+			<h1>Something</h1>
+		);
+	}
+}
